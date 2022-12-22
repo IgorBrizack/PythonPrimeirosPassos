ece4b54add5dca8bf2b8684a3485781874023177
Igor Brizack <igor_brizack@hotmail.com>
2022-12-21 21:54:15 -0300

    aplicando o format

--- aula6.py
+++ aula6.py
@@ -5,12 +5,18 @@
 # print("Seja muito bem vindo(a) {}".format(PESSOA))
 
 "colete o input 1"
-NUMERO1 = input('digite o primeiro número ')
+NUMERO1 = int(input('digite o primeiro número '))
 
 "colete o input2"
-NUMERO2 = input('digite o segundo número ')
+NUMERO2 = int(input('digite o segundo número '))
 
-print('aqui esta soma de n1 e n2: ', int(NUMERO1) + int(NUMERO2))
-print('aqui está a multiplicação de n1 e n2: ', int(NUMERO1) * int(NUMERO2))
-print('aqui está a subtração de n1 e n2: ', int(NUMERO1) - int(NUMERO2))
-print('aqui está a divisão de n1 e n2: ', int(NUMERO1) / int(NUMERO2))
+SOMA = NUMERO1 + NUMERO2
+SUB = NUMERO1 - NUMERO2
+DIV = NUMERO1 / NUMERO2
+MULT = NUMERO1 * NUMERO2
+
+print('aqui esta soma de {} e {}: {}'.format(NUMERO1, NUMERO2, SOMA))
+print('aqui está a multiplicação de {} e {}: {}'.format(NUMERO1, NUMERO2, MULT))
+print('aqui está a subtração de {} e {}: {}'.format(NUMERO1, NUMERO2, SUB))
+print('aqui está a divisão de {} e {}: {}'.format(NUMERO1, NUMERO2, DIV))
+
