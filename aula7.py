a6bcf387126a842a84b4d823c2eb5e453a56dd06
Igor Brizack <igor_brizack@hotmail.com>
2022-12-22 15:05:47 -0300

    atividade 007

---
+++ aula7.py
@@ -0,0 +1,13 @@
+"OPERAÇÕES ARITMÉTICAS"
+
+print('5 + 2 = ', 5+2)
+print('5 - 2 = ', 5-2)
+print('5 * 2 = ', 5*2)
+print('5 / 2 = ', 5/2)
+print('5 ^ 2 = ', 5**2)
+print('5 // 2 (inteiro) = ', 5//2)
+print('5 / 2 (resto da divisão) = ', 5%2)
+
+# "mostre na tela um programa que leia um número inteiro e mostre sucessor e seu antecessor"
+N1 = int(input("Digite um número "))
+print("o antecessor de {} é {} e o sucessor é {}".format(N1, N1 - 1, N1 + 1))
