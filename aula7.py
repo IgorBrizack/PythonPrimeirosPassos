4a7f7395467e4c5e53f9327d3ca1ada6cd504ea4
Igor Brizack <igor_brizack@hotmail.com>
2022-12-22 15:14:23 -0300

    desafio 7

--- aula7.py
+++ aula7.py
@@ -1,3 +1,4 @@
+import math
 "OPERAÇÕES ARITMÉTICAS"
 
 print('5 + 2 = ', 5+2)
@@ -10,4 +11,7 @@ print('5 / 2 (resto da divisão) = ', 5%2)
 
 # "mostre na tela um programa que leia um número inteiro e mostre sucessor e seu antecessor"
 N1 = int(input("Digite um número "))
-print("o antecessor de {} é {} e o sucessor é {}".format(N1, N1 - 1, N1 + 1))
+RAIZ_QUADRADA = math.sqrt(N1)
+print("O antecessor de {} é {} e o sucessor é {}".format(N1, N1 - 1, N1 + 1))
+print("O dobro de {} é {},  é {} e a raiz quadrada é {}".
+      format(N1, N1 * 2, N1 * 3, RAIZ_QUADRADA))
